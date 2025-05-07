ca6f29a390d848520af715891ebeb938b99deb7f
poojasaxena2000 <poojags2805@gmail.com>
2025-05-07 23:49:28 +0530

    done

---
+++ src/componets/HomePage.jsx
@@ -0,0 +1,55 @@
+
+  
+
+
+import { useEffect, useState } from "react";
+
+const Card = ({ name, flag }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "5px",
+        border: "1px solid gray",
+        borderRadius: "10px",
+        width: "200px",
+        height: "200px",
+        // marginTop:"20px"
+      }}
+    >
+      <img src={flag} alt={`flag of ${name}`} style={{ width: "80px", height: "80px", marginTop:"20px" }} />
+      <h2>{name}</h2>
+    </div>
+  );
+};
+
+const Endpoint = "https://xcountries-backend.azurewebsites.net/all";
+
+export default function HomePage() {
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    fetch(Endpoint)
+      .then((response) => response.json())
+      .then((data) => {
+        setApiData(data);
+      })
+      .catch((error) => console.error("Error fetching data", error));
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "10px",
+      }}
+    >
+      {apiData.map(({ name, flag, abbr }) => (
+        <Card key={abbr} name={name} flag={flag} />
+      ))}
+    </div>
+  );
+}
